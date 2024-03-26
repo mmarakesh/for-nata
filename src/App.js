67ac5c7a43c8b92a5d4067f4dc30f0818e80c127
NataRuto <13natik@mail.ru>
2024-03-26 22:06:59 +0300

    updated name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ name, setName ] = useState("Nata");
+  const updateName = () => {
+    setName("Marina");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello Nata!!!</p>
-        <button className='btn'>Please change the name after clicking!</button>
+        <p>Hello {name}!!!</p>
+        <button className='btn' onClick={() => updateName()}>Please change the name after clicking!</button>
       </header>
     </div>
   );
